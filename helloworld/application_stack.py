655078d6f3b9d772957fe60469a7299cf12ede0e
Stuart Graham <stuart@stuart-graham.com>
2021-08-06 12:26:23 +0100

    Initial commit

--- helloworld/application_stack.py
+++ helloworld/application_stack.py
@@ -1,8 +1,11 @@
 from aws_cdk import (
     core as cdk,
     aws_iam as iam,
-    aws_lambda as _lambda
+    aws_lambda as _lambda,
+    aws_apigatewayv2 as api_gw,
+    aws_apigatewayv2_integrations as api_gw_int
 )
+
 
 class HelloWorldStage(cdk.Stage):
     def __init__(self, scope: cdk.Construct, id: str, ecr_repo=None, **kwargs):
@@ -22,3 +25,15 @@ class HelloWorldStack(cdk.Stack):
         )
         hello_world_lambda.role.add_managed_policy(
             iam.ManagedPolicy.from_aws_managed_policy_name("AmazonEC2ContainerRegistryPowerUser"))
+
+        # APIGW
+        apigw_helloworld = api_gw.HttpApi(self, 'HelloWorld-APIGW-Http')
+        ## Lambda Integrations
+        hello_world_lambda_int = api_gw.LambdaProxyIntegration(
+            handler=hello_world_lambda)
+        ## Routes
+        apigw_helloworld.add_routes(
+            path='/',
+            methods=[api_gw.HttpMethod.GET],
+            integration=hello_world_lambda_int
+        )
